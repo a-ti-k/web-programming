c3006ef22801ec1b1a72b8c14b542549be2f5b32
a-ti-k <63835331+a-ti-k@users.noreply.github.com>
2023-02-27 13:08:38 +0600

    electricity

---
+++ index.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calculate electricity Bill</title>
+    <style type="text/css">
+        #page-wrap 
+        {
+            text-align:center;
+            font-size: 30px;
+        }
+        input
+        {
+            font-size:30ox;
+        }
+    </style>
+</head>
+
+<?php
+    $result_str = $result = '';
+    if(isset($_POST['unit-submit']))
+    {
+        $units=$_POST['units'];
+        if(!empty($units))
+        {
+            $result = calculate_bill($units);
+            $result_str = 'Total amount of ' . $units . ' units = ' . $result . 'Taka';
+
+        }
+    }
+
+    function calculate_bill($units)
+    {
+        $unit_cost_first = 3.50;
+        $unit_cost_second = 4.00;
+        $unit_cost_third = 5.20;
+        $unit_cost_forth = 6.50;
+
+        if($units <= 50)
+        {
+            $bill=$units*$unit_cost_first;
+        }
+        else if($units > 50 && $units <= 100)
+        {
+            $temp = 50 * $unit_cost_first;
+            $remaining_units = $units - 50;
+            $bill = $temp + ($remaining_units * $unit_cost_second);
+
+        }
+        else if($units > 100 && $units <= 200)
+        {
+            $temp = (50 * $unit_cost_first) + (100*$unit_cost_second);
+            $remaining_units = $units - 150;
+            $bill = $temp + ($remaining_units * $unit_cost_third);
+            
+        }
+        else
+        {
+            $temp = (50 * $unit_cost_first) + (100*$unit_cost_second) + (100*$unit_cost_third);
+            $remaining_units = $units - 250;
+            $bill = $temp + ($remaining_units * $unit_cost_forth);
+
+        }
+        return number_format((float)$bill,2,'.','');
+        
+    }
+
+?>
+
+<body>
+   <div id="page-wrap">
+    <h1>calculate electricity bill</h1>
+    <form action="" method="post">
+        <input type="number" min="0" name="units" placeholder="Please enter number of units">
+        <input type="submit" name="unit-submit" value="submit">
+    </form>
+   </div>
+
+    <?php
+        echo '<br>' . $result_str;
+    ?>
+</body>
+</html>
